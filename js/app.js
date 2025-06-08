91376b172e808d00bf10f696e526cf436c2a7f1a
Evande-blessing <blessingevande097@gmail.com>
2025-06-08 22:33:58 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav-links');
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('hidden');
+});
